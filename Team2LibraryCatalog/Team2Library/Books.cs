170f5cacec79a93d1bcb0d14543dfac52386e47f
Nathaniel Stripeik <nstripeik@gmail.com>
2017-05-03 20:16:49 -0500

    added class library

---
+++ Team2LibraryCatalog/Team2Library/Books.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Team2Library
+{
+    public class Books
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+
+        public bool Checkout { get; set; }
+
+        public string Location { get; set; }
+         public List<string> Author { get; set; }
+
+        public string Description { get; set; }
+        
+    }
+}
