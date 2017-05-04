f6b52d46864619e068b487f9c4cf3743c257a844
Nathaniel Stripeik <nstripeik@gmail.com>
2017-05-03 19:46:36 -0500

    Created solution

---
+++ Team2LibraryCatalog/Team2LibraryCatalog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Team2LibraryCatalog.MvcApplication" Language="C#" %>
---
+++ Team2LibraryCatalog/Team2LibraryCatalog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Team2LibraryCatalog.Startup))]
+namespace Team2LibraryCatalog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
